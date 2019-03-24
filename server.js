82a2d7c082cd80b81e15353d9ed85711fc88052a
lmccray <lmccray@uhin.org>
2019-03-24 14:22:42 -0600

    fixes

--- server.js
+++ server.js
@@ -2,30 +2,21 @@ var express = require("express");
 var logger = require("morgan");
 var mongoose = require("mongoose");
 var exphbs = require("express-handlebars");
-
-// Our scraping tools
-// Axios is a promised-based http library, similar to jQuery's Ajax method
-// It works on the client and on the server
 var axios = require("axios");
 
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/article-scraper";
 
-// Require all models
-//var db = require("./models");
-mongoose.connect("mongodb://localhost/article-scraper", { useNewUrlParser: true });
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
 
-var PORT = 3003;
+var PORT = process.env.PORT || 3000;
 
 // Initialize Express
 var app = express();
 
-// Configure middleware
-
-// Use morgan logger for logging requests
 app.use(logger("dev"));
-// Parse request body as JSON
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-// Make public a static folder
+
 
 // Handlebars
 app.engine(
@@ -42,6 +33,6 @@ require("./routes/apiRoutes")(app);
 require("./routes/htmlRoutes")(app);
 
 // Start the server
-app.listen(PORT, function() {
+app.listen(PORT, function () {
   console.log("App running on port " + PORT + "!");
 });
